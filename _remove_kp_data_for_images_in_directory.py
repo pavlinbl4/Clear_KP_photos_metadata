6949b5e8956e6bd4adec35a6cf3891935d018924
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-04-20 23:38:55 +0300

    make function more universal

--- _remove_kp_data_for_images_in_directory.py
+++ _remove_kp_data_for_images_in_directory.py
@@ -1,9 +1,7 @@
 from metadata_tools.clear_from_kp import clear_image_metadata_from_kp_info
 from tk_tools import select_folder_via_gui
-from find_file_hdd import action_with_image_files_in_directory
+from find_file_hdd import find_files_in_dir_with_extension
 from loguru import logger
-
-
 
 logger.add("output.log", format="{time} {level} {message}", level="ERROR")
 logger.add("output.log", format="{time} {level} {message}", level="INFO")
@@ -14,7 +12,13 @@ def main():
     initial_dir = '/Volumes/big4photo-4/KP_yellow'
     path_to_folder = select_folder_via_gui(initial_dir)
 
-    action_with_image_files_in_directory(path_to_folder, clear_image_metadata_from_kp_info)
+    # find all files in directory and subdirectories
+    images_list = find_files_in_dir_with_extension(path_to_folder,
+                                                   files_extensions=('jpg', 'jpeg', 'dng', 'nef', 'orf'))
+
+    # edit metadate in images
+    for image in images_list:
+        clear_image_metadata_from_kp_info(image)
 
 
 if __name__ == '__main__':
