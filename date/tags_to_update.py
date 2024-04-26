a3e91cfbfbceaf15cb875c77d797ecf3d61acaea
evgeniy <pavlinbl4@gmail.com>
2024-04-26 16:40:37 +0300

    tags library

---
+++ date/tags_to_update.py
@@ -0,0 +1,29 @@
+tags = {'XMP:Rights': 'Pavlenko Evgeniy',
+        'XMP:Credit': 'Pavlenko Evgeniy',
+        'XMP:Creator': 'Pavlenko Evgeniy',
+        'XMP:Title': '',
+        'XMP:Label': "Green",
+        'IPTC:By-line': 'Pavlenko Evgeniy',
+        'IPTC:Credit': 'Pavlenko Evgeniy',
+        'IPTC:CopyrightNotice': 'Pavlenko Evgeniy',
+        'IPTC:ObjectName': '',
+        }
+
+
+important_data = [
+    # 'IPTC:Country-PrimaryLocationCode',
+    # 'IPTC:By-line',
+    # 'IPTC:Caption-Abstract',
+    # 'IPTC:CopyrightNotice',
+    'XMP:Headline',
+    'XMP:Description',
+    'XMP:Subject',
+    'XMP:Country',
+    'XMP:Label',
+    'XMP:Rating',
+    'XMP:Urgency',
+    'XMP:Credit',
+    'XMP:Country',
+    'XMP:City',
+    'XMP:Subject',
+    'XMP:Instructions']
