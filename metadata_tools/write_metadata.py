2663d48f5637e5e77b81ff57c8901c826422b769
evgeniy <pavlinbl4@gmail.com>
2024-04-26 14:00:18 +0300

    added function to write metadate from dict

--- metadata_tools/write_metadata.py
+++ metadata_tools/write_metadata.py
@@ -23,3 +23,13 @@ def write_edited_tags(xmp_tags):
                         params=["-P", "-overwrite_original"])
     except ExifToolExecuteError as e:
         logger.info(f'{e}\n{xmp_tags}')
+
+
+def add_xmp_date_to_image_file(path_to_file, xmp_date_dict):
+    try:
+        with exiftool.ExifToolHelper() as et:
+            et.set_tags(path_to_file,
+                        xmp_date_dict,
+                        params=["-P", "-overwrite_original"])
+    except ExifToolExecuteError as e:
+        logger.info(f'{e}')
