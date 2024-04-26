3c99cd222c449f01d7de661618cfe539add2e106
evgeniy <pavlinbl4@gmail.com>
2024-04-26 16:11:27 +0300

    refactoring

--- metadata_tools/clear_from_kp.py
+++ metadata_tools/clear_from_kp.py
@@ -1,16 +1,16 @@
-from metadata_tools.read_metadata import read_image_metadate
+from metadata_tools.read_metadata_from_image import read_image_metadate
 from metadata_tools.write_metadata import write_edited_tags
 from regex_tools import modify_caption
 from icecream import ic
 
 
-def clear_image_metadata_from_kp_info(path_to_image_file):
+def clear_image_metadata_from_kp_info(path_to_image_file: str):
     # read metadata
-    image_metadata = read_image_metadate(path_to_image_file)
+    image_metadata: dict = read_image_metadate(path_to_image_file)
     # ic(image_metadata)
 
     # modify caption
-    updated_metadata = modify_caption(image_metadata)
+    updated_metadata: dict = modify_caption(image_metadata)
     # ic(updated_metadata)
 
     # save updates metadata to file
