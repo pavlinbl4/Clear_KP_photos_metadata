60611b24710878a39dee0ddf13bfb0b8bd406e4c
evgeniy <pavlinbl4@gmail.com>
2024-04-26 16:42:55 +0300

    function to clear metadata

--- metadata_tools/metadate_optimization.py
+++ metadata_tools/metadate_optimization.py
@@ -1,11 +1,10 @@
-# read metadate from image and save it to pickle file
-from icecream import ic
+# read metadata from image and save it to pickle file
 
 from metadata_tools.clear_image_metadate import clear_all_metadate
-from metadata_tools.read_metadata_from_image import save_metadate_in_pickle_file, read_image_metadate
-from metadata_tools.read_metadate_from_csv_file import read_important_metadate_from_pickle_file
+
 from metadata_tools.unwanted_tags import clear_unwanted_tags
 from metadata_tools.write_metadata import add_xmp_date_to_image_file
+from pickle_files.work_with_pickle import save_metadate_in_pickle_file, read_important_metadate_from_pickle_file
 from regex_tools import modify_caption
 
 
@@ -13,7 +12,7 @@ def metadate_clearing(path_to_image_file):
     # read_image_metadate(path_to_image_file)
     path_to_pickle_file = save_metadate_in_pickle_file(path_to_image_file)
 
-    # clear all metadate in image
+    # clear all metadata in image
     clear_all_metadate(path_to_image_file)
 
     # read only important date from pickle file
@@ -28,9 +27,4 @@ def metadate_clearing(path_to_image_file):
     # write this date to image
     add_xmp_date_to_image_file(path_to_image_file, updated_metadata)
 
-    ic(read_image_metadate(path_to_image_file))
-
-
-if __name__ == '__main__':
-    image2 = '/Volumes/big4photo/_PYTHON/Clear_KP_photos_metadata/tests/test_files/20211201PEV_2795-Edit.JPG'
-    metadate_clearing(image2)
+    # ic(read_image_metadate(path_to_image_file))
