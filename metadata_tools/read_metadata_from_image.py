056265ad4aea6dcf2870d9b6bf3618448a87e6e0
evgeniy <pavlinbl4@gmail.com>
2024-04-26 15:55:19 +0300

    refactoring

--- metadata_tools/read_metadata_from_image.py
+++ metadata_tools/read_metadata_from_image.py
@@ -9,7 +9,6 @@ import pickle
 def read_image_metadate(path_to_image_file: str) -> dict:
     with exiftool.ExifToolHelper() as et:
         image_metadate = et.get_metadata(path_to_image_file)[0]
-        # logger.info()
         return image_metadate
 
 
@@ -21,7 +20,6 @@ def read_tag(path_to_image_file):
 # function to read metadate from image and save it to csv file
 def save_metadate_in_file(path_to_image_file: str):
     metadate_dict = read_image_metadate(path_to_image_file)
-
     with open(f'{metadate_dict["File:FileName"]}_metadate.csv', 'w', newline='') as csv_file:
         cvs_writer = csv.writer(csv_file)
         for key, value in metadate_dict.items():
@@ -31,16 +29,17 @@ def save_metadate_in_file(path_to_image_file: str):
 
 def save_metadate_in_pickle_file(path_to_image_file: str):
     metadate_dict = read_image_metadate(path_to_image_file)
-    with open('metadate.pickle', 'wb') as pickle_file:
+    path_to_pickle_file = '../metadate.pickle'
+    with open(path_to_pickle_file, 'wb') as pickle_file:
         pickle.dump(metadate_dict, pickle_file)
+    return path_to_pickle_file
 
 
 if __name__ == '__main__':
-    image = '../tests/test_files/KSP_015561_00123_1h.jpg'
-    image2 = '../tests/test_files/20231214EPAV9435.jpg'
+    image = '../tests/test_files/20211201PEV_2795-Edit.JPG'
+    image2 = '../tests/test_files/all_fields.JPG'
     image3 = '../tests/test_files/20211201PEV_2795-Edit.JPG'
     all_fields = '../tests/test_files/all_fields.JPG'
-    ic(read_image_metadate(all_fields
-                           ))
+    read_image_metadate(image2)
     # save_metadate_in_file(all_fields)
-    save_metadate_in_pickle_file(image3)
+    # save_metadate_in_pickle_file(image3)
