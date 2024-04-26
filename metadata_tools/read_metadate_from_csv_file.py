c8a45404df0e22401614fc39457463e0d109cb2a
evgeniy <pavlinbl4@gmail.com>
2024-04-26 14:22:38 +0300

    read from pickle file only important metadate

--- metadata_tools/read_metadate_from_csv_file.py
+++ metadata_tools/read_metadate_from_csv_file.py
@@ -3,30 +3,34 @@ function will read only information for good caption of image from csv file
 """
 
 import csv
+import pickle
+
 from icecream import ic
 
 from metadata_tools.write_metadata import add_xmp_date_to_image_file
+
+important_data = [
+    # 'IPTC:Country-PrimaryLocationCode',
+    # 'IPTC:By-line',
+    # 'IPTC:Caption-Abstract',
+    # 'IPTC:CopyrightNotice',
+    'XMP:Headline',
+    'XMP:Description',
+    'XMP:Subject',
+    'XMP:Country',
+    'XMP:Label',
+    'XMP:Rating',
+    'XMP:Urgency',
+    'XMP:Credit',
+    'XMP:Country',
+    'XMP:City',
+    'XMP:Subject',
+    'XMP:Instructions']
 
 
 def read_important_metadate_from_csv_file(csv_file_with_xmp):
     with open(csv_file_with_xmp, 'r') as csv_file:
         xmp_date_dict = {}
-        important_data = ['IPTC:Country-PrimaryLocationCode',
-                          'IPTC:By-line',
-                          'IPTC:Caption-Abstract',
-                          'IPTC:CopyrightNotice',
-                          'XMP:Headline',
-                          'XMP:Description',
-                          'XMP:Subject',
-                          'XMP:Country',
-                          'XMP:Label',
-                          'XMP:Rating',
-                          'XMP:Urgency',
-                          'XMP:Credit',
-                          'XMP:Country',
-                          'XMP:City',
-                          'XMP:Subject',
-                          'XMP:Instructions']
         for line in csv_file:
             # print(line.strip().split(','))
             if line.strip().split(',')[0] in important_data:
@@ -35,9 +39,22 @@ def read_important_metadate_from_csv_file(csv_file_with_xmp):
                 xmp_date_dict[line.strip().split(',')[0]] = line.strip().split(',')[1]
         return xmp_date_dict
 
+
+def read_important_metadate_from_pickle_file(path_to_pickle_file):
+    xmp_date_dict = {}
+    with open(path_to_pickle_file, 'rb') as pickle_file:
+        pickle_dict = pickle.load(pickle_file)
+    for key in pickle_dict:
+        if key in important_data:
+            xmp_date_dict[key] = pickle_dict[key]
+    return xmp_date_dict
+
+
 if __name__ == '__main__':
-    xmp_date = read_important_metadate_from_csv_file('../metadata_tools/all_fields.JPG_metadate.csv')
+    # xmp_date = read_important_metadate_from_csv_file('../metadata_tools/all_fields.JPG_metadate.csv')
+    xmp_date = read_important_metadate_from_pickle_file('../metadata_tools/metadate.pickle')
     ic(xmp_date)
 
-    # add_xmp_date_to_image_file('/Volumes/big4photo/_PYTHON/Clear_KP_photos_metadata/tests/test_files/20211201PEV_2795-Edit.JPG',
-    #                            xmp_date)
+    add_xmp_date_to_image_file(
+        '/Volumes/big4photo/_PYTHON/Clear_KP_photos_metadata/tests/test_files/20211201PEV_2795-Edit.JPG',
+        xmp_date)
