9535c43f94174c869913f8ba0e041eb42f86fb30
evgeniy <pavlinbl4@gmail.com>
2024-05-06 23:07:20 +0300

    exiftool_args = ["-m", "-overwrite_original"]  Разберем каждый аргумент:
    "-m": Этот аргумент указывает exiftool перезаписывать существующие метаданные в файле изображения. Без этого аргумента exiftool добавит новые метаданные к уже существующим, что может привести к дублированию информации.
    "-overwrite_original": Этот аргумент указывает exiftool перезаписать оригинальный файл изображения с новыми метаданными. Без этого аргумента exiftool создаст новый файл с обновленными метаданными, оставив исходный файл нетронутым.

--- metadata_tools/write_metadata.py
+++ metadata_tools/write_metadata.py
@@ -9,7 +9,15 @@ def add_xmp_date_to_image_file(path_to_file, xmp_date_dict):
             et.set_tags(path_to_file,
                         xmp_date_dict,
                         # params=["-P", "-overwrite_original"]
+                        params=["-m", "-overwrite_original"]
 
                         )
     except ExifToolExecuteError as e:
         logger.info(f'{e}')
+
+
+
+if __name__ == '__main__':
+    add_xmp_date_to_image_file('/Volumes/big4photo-4/to_trash',
+                               {'XMP:Label': "Green"})
+
