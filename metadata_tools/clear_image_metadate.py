49a03f198a6b820f20bc33f43fc80b215cf2ae9e
evgeniy <pavlinbl4@gmail.com>
2024-05-07 17:52:13 +0300

    added param "-overwrite_original" to escape double images

--- metadata_tools/clear_image_metadate.py
+++ metadata_tools/clear_image_metadate.py
@@ -4,4 +4,4 @@ import exiftool
 def clear_all_metadate(path_to_image_file):
     with exiftool.ExifTool() as et:
         # Remove all metadata from an image
-        et.execute("-all=", path_to_image_file)
+        et.execute("-all=", path_to_image_file, "-overwrite_original")
